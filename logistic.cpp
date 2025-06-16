1130fe5fd0214af375d00481f03b4bfcbb540220
Kostya <Kostya>
2025-06-16 20:47:26 +0500

    commit

---
+++ logistic.cpp
@@ -0,0 +1,15 @@
+#include "logistic.h"
+
+void Main_menu() {
+    char i;
+    names();
+    cin >> i;
+    if (i == 1){
+        selection();
+    }
+    if (i == 2) {
+        break;
+    }
+}
+
+
---
+++ logistic.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Pedro{
+    int hp = 3;
+    int armor = 0;
+
+};
+
+void Main_menu();
+void hp_minus(Pedro hp);
