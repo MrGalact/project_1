d5e3610072579515dd19f2a5ab8bc65a69556341
Mark <Mark>
2025-06-19 20:37:32 +0500

    ya dobavil ui code

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
-#include <iostream>
+#include "visual.h"
 #include "logistic.h"
 
-using namespace std;
-
-string asd = "Привет";
-
-int main() {
+int main(){
     setlocale(LC_ALL, "ru_RU.UTF-8");
+    names();
+
     GameInfo info();
+
+    int answer = otvet();
+
+    while(info.is_game_over == false){
+        cout << "ГЛАВА: 1";
+
+        for(int i = 0; i < 3; i++){
+        selection(quest[i], get_otvet_a[i], get_otvet_b[i]);
+        int ans = otvet();
+        info.quest_changes(i, ans);
+    }
+  
+    cout << "Вы прошли игру, Вернув человеку его кошелек, +реп +одна золотая моента от человека +вайб +совесть";
+
+}
+cout << death_message;
 }
---
+++ visual.cpp
@@ -0,0 +1,29 @@
+#include "visual.h"
+
+void names(){
+    cout << "===========================\n"; 
+    cout << "|     PEDRO SURVIVE       |\n";
+    cout << "===========================\n"; 
+    cout << "|   1. PLAY  |   2. EXIT  |\n";
+    cout << "===========================\n";
+}
+
+void selection(string c, string a, string b){
+cout << c << endl;
+cout << "1. " << a << endl;
+cout << "2. " << b << endl;
+}
+
+
+
+
+
+
+int otvet(){
+    int ans = 0;
+    cin >> ans;
+    return ans;
+}
+
+
+
